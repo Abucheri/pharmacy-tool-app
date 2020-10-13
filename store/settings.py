556dc4078722790fe07e8c895dc3a6ab4c889ff6
O'Brien <abucheriobrien@gmail.com>
2020-10-13 16:14:18 +0300

    made some changes concerning static files in settings file

--- store/settings.py
+++ store/settings.py
@@ -139,18 +139,19 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
-PROJECT_ROOT = os.path.join(os.path.abspath(__file__))  # for heroku
-STATIC_ROOT = os.path.join(PROJECT_ROOT, 'staticfiles')  # for heroku
+# PROJECT_ROOT = os.path.join(os.path.abspath(__file__))  # for heroku
+# STATIC_ROOT = os.path.join(PROJECT_ROOT, 'staticfiles')  # for heroku
 STATIC_URL = '/static/'
 
 # Extra lookup directories for collectstatic to find static files, for heroku
-STATICFILES_DIRS = (
-    os.path.join(PROJECT_ROOT, 'static'),
-)
+# STATICFILES_DIRS = (
+#     os.path.join(PROJECT_ROOT, 'static'),
+# )
 
 # Add configuration for static files storage using whitenoise
 # STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+
 CRISPY_TEMPLATE_PACK = 'bootstrap4'
 
 ACCOUNT_ACTIVATION_DAYS = 7  # One-week activation window
