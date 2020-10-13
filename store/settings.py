08970250eeac5a1c2b5f2b0789505888f666beb9
O'Brien <abucheriobrien@gmail.com>
2020-10-13 16:59:14 +0300

    made some changes to the staticfiles setting in settings.py

--- store/settings.py
+++ store/settings.py
@@ -140,13 +140,14 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 # PROJECT_ROOT = os.path.join(os.path.abspath(__file__))  # for heroku
-# STATIC_ROOT = os.path.join(PROJECT_ROOT, 'staticfiles')  # for heroku
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')  # for heroku
 STATIC_URL = '/static/'
 
 # Extra lookup directories for collectstatic to find static files, for heroku
-# STATICFILES_DIRS = (
-#     os.path.join(PROJECT_ROOT, 'static'),
-# )
+# Extra places for collectstatic to find static files.
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, 'static'),
+)
 
 # Add configuration for static files storage using whitenoise
 # STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
